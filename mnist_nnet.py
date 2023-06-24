d8ed3dea1e823be5f888ff100f517aa64b2f3156
Daniel Camarena <danielcamarena@Daniels-MacBook-Pro.local>
2023-06-24 12:46:33 -0700

    first commit

---
+++ mnist_nnet.py
@@ -0,0 +1,112 @@
+from keras import models
+from keras import layers
+from keras.datasets import mnist
+from keras.utils import to_categorical
+import numpy as np
+from matplotlib import pyplot as plt
+
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+train_images = train_images.reshape(60000, 28*28)
+test_images = test_images.reshape(10000, 28*28)
+train_data = np.array(train_images).T
+test_data = np.array(test_images).T
+train_data = train_data/255
+test_data = test_data/255
+train_labels = np.array(train_labels).T
+test_labels = np.array(test_labels).T
+_,m = train_data.shape
+
+
+
+def init_params():
+    W1 = np.random.rand(10, 784) - 0.5
+    b1 = np.random.rand(10, 1) - 0.5
+    W2 = np.random.rand(10, 10) - 0.5
+    b2 = np.random.rand(10, 1) - 0.5
+    return W1, b1, W2, b2
+
+def ReLU(Z):
+    return np.maximum(Z,0)
+
+def softmax(Z):
+    A = np.exp(Z)/sum(np.exp(Z))
+    return A
+
+def forward_prop(W1, b1, W2, b2, X):
+    Z1 = W1.dot(X) + b1
+    A1 = ReLU(Z1)
+    Z2 = W2.dot(A1) + b2
+    A2 = softmax(Z2)
+    return Z1, A1, Z2, A2
+
+def ReLU_deriv(Z):
+    return Z > 0
+
+def one_hot(Y):
+    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
+    one_hot_Y[np.arange(Y.size), Y] = 1
+    one_hot_Y = one_hot_Y.T
+    return one_hot_Y
+
+def backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
+    one_hot_Y = one_hot(Y)
+    dZ2 = A2 - one_hot_Y
+    dW2 = 1/m * dZ2.dot(A1.T)
+    db2 = 1/m * np.sum(dZ2)
+    dZ1 = W2.T.dot(dZ2) * ReLU_deriv(Z1)
+    dW1 = 1 / m * dZ1.dot(X.T)
+    db1 = 1 / m * np.sum(dZ1)
+    return dW1, db1, dW2, db2
+
+def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
+    W1 = W1 - alpha*dW1
+    b1 = b1 - alpha*db1
+    W2 = W2 - alpha*dW2
+    b2 = b2 - alpha*db2
+    return W1, b1, W2, b2
+
+def get_predictions(A2):
+    return np.argmax(A2,0)
+
+def get_accuracy(predictions, Y):
+    print(predictions, Y)
+    return np.sum(predictions == Y)/Y.size
+
+def gradient_descent(X, Y, alpha, iterations):
+    W1, b1, W2, b2 = init_params()
+    for i in range(iterations):
+        Z1, A1, Z2, A2 = forward_prop(W1, b1, W2, b2, X)
+        dW1, db1, dW2, db2 = backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y)
+        W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
+        if i % 10 == 0:
+            print("Iteration: ", i)
+            predictions = get_predictions(A2)
+            print(get_accuracy(predictions, Y))
+    return W1, b1, W2, b2
+    
+W1, b1, W2, b2 = gradient_descent(train_data, train_labels, 0.10, 500)
+
+def make_predictions(X, W1, b1, W2, b2):
+    _,_,_,A2 = forward_prop(W1, b1, W2, b2, X)
+    return get_predictions(A2)
+
+def test_prediction(index, W1, b1, W2, b2):
+    current_image = train_data[:, index, None]
+    prediction = make_predictions(current_image, W1, b1, W2, b2)
+    label = train_labels[index]
+    print("Prediction: ", prediction)
+    print("Label: ", label)
+    current_image = current_image.reshape((28, 28)) * 255
+    plt.gray()
+    plt.imshow(current_image, interpolation='nearest')
+    plt.show()
+    
+test_prediction(0, W1, b1, W2, b2)
+test_prediction(1, W1, b1, W2, b2)
+test_prediction(2, W1, b1, W2, b2)
+test_prediction(3, W1, b1, W2, b2)
+    
+
+    
+    
